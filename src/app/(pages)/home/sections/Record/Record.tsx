d5de3b1555d8ba30e056a12e441b0c09c2a585c0
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-19 02:48:59 +0900

    feat: implement CardNumber in Record section(#3)

---
+++ src/app/(pages)/home/sections/Record/Record.tsx
@@ -0,0 +1,9 @@
+import CardNumberRow from "./components/CardNumberRow";
+
+export default function Record() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-screen">
+            <CardNumberRow />
+        </div>
+    );
+}
