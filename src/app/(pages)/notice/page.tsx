02a7e141984d378536ad1b7fd94f09eb1ec2d19a
dohyeoplim <dhlim0817@gmail.com>
2025-07-03 03:55:14 +0000

    Merge pull request #106 from LIKELION-SEOULTECH/fixes-misc
    fix: add missing revalidate time to notice page

--- src/app/(pages)/notice/page.tsx
+++ src/app/(pages)/notice/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import NoticeBanner from "./components/NoticeBanner";
 import NoticeList from "./components/NoticeList";
 
+export const revalidate = 60;
+
 export default function NoticePage() {
     return (
         <>
